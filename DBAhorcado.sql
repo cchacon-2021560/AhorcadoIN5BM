0752253bde0d98a6ee3a63173fafcf7efb610871
cchacon-2021560 <cchacon-2021560@kinal.edu.gt>
2025-09-22 22:04:46 -0600

    Fix: Corregir base de datos

--- DBAhorcado.sql
+++ DBAhorcado.sql
@@ -4,7 +4,7 @@ use db_Ahorcado;
  
 create table Palabras(
 	codigo_palabra int auto_increment,
-	nombre varchar(20) not null,
+	nombre varchar(20) unique not null,
 	cualidad_uno varchar(100) not null,
 	cualidad_dos varchar(100) not null,
 	cualidad_tres varchar(100) not null,
@@ -70,7 +70,7 @@ create procedure sp_ActualizarPalabra(
 	where codigo_palabra = codP;
     end$$
 delimiter ;
-call sp_ActualizarPalabra(1, "Elefantin", "Grande", "Vive en manadas", "Tiene trompa");
+call sp_ActualizarPalabra(1, "Elefante", "Grande", "Vive en manadas", "Tiene trompa");
 drop procedure sp_ActualizarPalabra;
  
 delimiter $$
@@ -93,7 +93,7 @@ create procedure sp_AgregarUsuario(
 	end$$
 delimiter ;
 
-call sp_AgregarUsuario('pepito@gmail.com', 'onetu');
+call sp_AgregarUsuario('mickey@gmail.com', 'onetu');
 call sp_AgregarUsuario('andres@gmail.com', 'trifor');
 call sp_AgregarUsuario('bartolome@gmail.com', 'siseven');
 
@@ -133,7 +133,6 @@ create procedure sp_EliminarUsuario(
 	end$$
 delimiter ;
 
-call sp_EliminarUsuario(2);
 
 call sp_ListarUsuarios();
 call sp_ListarPalabras();
